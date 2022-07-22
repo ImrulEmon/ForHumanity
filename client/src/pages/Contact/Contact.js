95cfb886d327d062613190a1ff7dc98abaa16396
ImrulEmon <emimrulemon@gmail.com>
2022-07-22 16:36:24 +0600

    Contact-Feedback from(react hook form)

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import { useTabtitle } from '../../hooks/useTabtitle';
+import React, { useState } from "react";
+import { useTabtitle } from "../../hooks/useTabtitle";
+import { useForm } from "react-hook-form";
+import Form from "react-bootstrap/Form";
 
 const Contact = () => {
-  useTabtitle('Contact');
+  useTabtitle("Contact");
+
+  // const [feedback,setFeedback]=useState({});
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+  console.log(errors);
+
   return (
     <div className="container">
-    
-      <h1 className="text-center" style={{paddingTop: "30%"}}>
-        Contact Us
-      </h1>
-      
+      <div>
+        <h1 className="text-center my-5">Contact Us</h1>
+        {/* <form onSubmit={handleSubmit(onSubmit)}>
+  <input
+    type="email"
+    placeholder="Email"
+    {...register("Email", { required: true })}
+  />
+  <textarea {...register("Feedback", { required: true })} />
+
+  <input type="submit" />
+</form> */}
+
+        <Form className="my-5" onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              {...register("Email", { required: true })}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Feedback</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Please Give Your Feedback"
+              rows={3}
+              {...register("Feedback", { required: true })}
+            />
+          </Form.Group>
+          <input className="btn btn-dark" type="submit" />
+        </Form>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Contact;
