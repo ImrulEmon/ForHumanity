5f6694073f746ca84404067c90c39cc52f7b2fe9
ImrulEmon <emimrulemon@gmail.com>
2022-08-09 19:56:17 +0600

    Add Volunteer/member POST API

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,40 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+const cors = require("cors");
 const app = express();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkpyk.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("forHumanity");
-      const activitiesCollection = database.collection("activities");
-     
-      // GET API - SHOW EVENTS
-      app.get('/events',async(req,res)=>{
-        const query = {}
-        const cursor = activitiesCollection.find(query);
-        const events=await cursor.toArray();
-        res.send(events);
-      })
-      
-    } finally {
-     // await client.close();
-    }
+  try {
+    await client.connect();
+
+    const database = client.db("forHumanity");
+    const activitiesCollection = database.collection("activities");
+    const membersCollection = database.collection('member');
+
+    // GET API - SHOW EVENTS
+    app.get("/events", async (req, res) => {
+      const query = {};
+      const cursor = activitiesCollection.find(query);
+      const events = await cursor.toArray();
+      res.send(events);
+    });
+
+    // POST API - ADD VOLUNTEER
+    app.post("/member",async(req,res)=>{
+      const member = req.body;
+      //console.log(service);
+      const result = await membersCollection.insertOne(member);
+      //console.log(result);
+      res.json(result);
+    })
+
+    // GET API - ADD VOLUNTEER
+   app.get('/volunteer',async(req,res)=>{
+      const query = {};
+      const cursor = await membersCollection.find(query);
+      const members = await cursor.toArray();
+      res.send(members);
+   })
+
+  } 
+  finally {
+    //await client.close();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send('ForHumanity Server is running...')
-})
+app.get("/", (req, res) => {
+  res.send("ForHumanity Server is running...");
+});
 
-app.listen(port,()=>{
-    console.log(`Ok ${port}!!!`);
-})
+app.listen(port, () => {
+  console.log(`Ok Port : ${port}!!!`);
+});
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,78 +1,102 @@
 import React from "react";
+import axios from "axios";
 import { Card, Container, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import "./Register.css";
-import { useTabtitle } from '../../hooks/useTabtitle';
+import { useTabtitle } from "../../hooks/useTabtitle";
 
 const Register = () => {
   useTabtitle("Volunteer Register");
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    //,formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+
+  const onSubmit = (data) => {
+    //console.log(data);
+    fetch("http://localhost:5000/member", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status==200) {
+        alert("Added Successfully");
+        reset();
+      }
+    });
+  };
+  //console.log(errors);
+
   return (
     <Container style={{ minHeight: "100vh" }}>
-         <div className="w-100 mx-auto my-5" style={{ maxWidth: "400px" }}>
-    <Card>
-      <Card.Body>
-        <legend className="text-center py-4">Register as a Volunteer</legend>
-        <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group id="firstName" className="my-2">
-            <Form.Label>Full Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Full Name"
-              {...register("FullName", {maxLength: 80 })}
-            required/>
-          </Form.Group>
+      <div className="w-100 mx-auto my-5" style={{ maxWidth: "400px" }}>
+        <Card>
+          <Card.Body>
+            <legend className="text-center py-4">
+              Register as a Volunteer
+            </legend>
+            <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group id="firstName" className="my-2">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Full Name"
+                  {...register("FullName", { maxLength: 80 })}
+                  required
+                />
+              </Form.Group>
 
-          <Form.Group id="email" className="my-2">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Email"
-              {...register("Email", {pattern: /^\S+@\S+$/i })}
-              required/>
-          </Form.Group>
+              <Form.Group id="email" className="my-2">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  {...register("Email", { pattern: /^\S+@\S+$/i })}
+                  required
+                />
+              </Form.Group>
 
-          <Form.Group id="date" className="my-2">
-            <Form.Label>Date</Form.Label>
-            <Form.Control
-              type="datetime"
-              placeholder="Date"
-              {...register("Date", {maxLength: 100 })}
-              required/>
-          </Form.Group>
+              <Form.Group id="date" className="my-2">
+                <Form.Label>Date</Form.Label>
+                <Form.Control
+                  type="datetime"
+                  placeholder="Date"
+                  {...register("Date", { maxLength: 100 })}
+                  required
+                />
+              </Form.Group>
 
-          <Form.Group id="mobile" className="my-2">
-            <Form.Label>Mobile Number</Form.Label>
-            <Form.Control
-              type="tel"
-              placeholder="Mobile number"
-              {...register("MobileNumber", {
-                
-                minLength: 6,
-                maxLength: 12,
-              })}
-              required/>
-          </Form.Group>
+              <Form.Group id="mobile" className="my-2">
+                <Form.Label>Mobile Number</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="Mobile number"
+                  {...register("MobileNumber", {
+                    minLength: 6,
+                    maxLength: 12,
+                  })}
+                  required
+                />
+              </Form.Group>
 
-          <Form.Group id="event" className="my-2">
-            <Form.Label>Event</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Event"
-              {...register("Event", {maxLength: 80 })}
-            required/>
-          </Form.Group>
-          <input className="btn btn-dark my-3" type="submit" />
-        </form>
-      </Card.Body>
-    </Card>
-    </div>
+              <Form.Group id="event" className="my-2">
+                <Form.Label>Event</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Event"
+                  {...register("Event", { maxLength: 80 })}
+                  required
+                />
+              </Form.Group>
+              <input className="btn btn-dark my-3" type="submit" />
+            </form>
+          </Card.Body>
+        </Card>
+      </div>
     </Container>
   );
 };
