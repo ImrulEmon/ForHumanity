31dcd4fef251ad10dbc04a9af04744ad40289c04
ImrulEmon <emimrulemon@gmail.com>
2022-08-16 01:50:00 +0600

    redirect after login

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
 
 const PrivateRoute = ({ children, ...rest }) => {
+  const location = useLocation();
   const { user, isLoading } = useAuth();
   if (isLoading) {
     return <Spinner className="my-5" animation="border" variant="danger" />;
   }
-  return user.displayName ? children : <Navigate to="/login" />;
+  return user.displayName ? children : <Navigate to="/login" replace={true} state={{from:location}}/>;
 };
 
 export default PrivateRoute;
