15c3333b52eeb59588399031672a7dc03412df93
ImrulEmon <emimrulemon@gmail.com>
2022-08-21 01:02:02 +0600

    User Event Table

--- client/src/components/UserCard/UserCard.js
+++ client/src/components/UserCard/UserCard.js
@@ -2,29 +2,16 @@ import React from "react";
 import "./UserCard.css";
 
 const UserCard = (props) => {
- const{Date,Event,FullName,email}=props.myevent;
+ const{index,Date,Event,FullName,email,_id}=props?.myevent;
   return (
-    <div className="container col-12 col-md-6 col-lg-3">
-      <div className="card d-flex align-items-center justify-content-center">
-        <div className="slide slide1">
-          <div className="content">
-            <div className="icon">
-              <i className="fa fa-user-circle" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-
-        <div className="slide slide2">
-          <div className="content">
-            <h4>Hello there! {FullName}</h4>
-            <p>Event : {Event}</p>
-            <p>{Date}</p>
-            <p>Email:{email}</p>
-            <p>Trust yourself and keep going.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <tr>
+      
+      <td>{FullName}</td>
+      <td>{Event}</td>
+      <td>{Date}</td>
+      <td>{email}</td>
+      <td><i className="fas fa-trash"></i></td>
+    </tr>
   );
 };
 
--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -1,70 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
-import UserCard from '../../components/UserCard/UserCard';
-import useAuth from '../../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import UserCard from "../../components/UserCard/UserCard";
+import useAuth from "../../hooks/useAuth";
 import "./user.css";
 
 const User = () => {
-    const {user,isLoading,setIsLoading}=useAuth({});
-    const [myevents,setMyevents]=useState([]);
-    const [loading,setLoading]=useState(true);
-    const navigate = useNavigate();
+  const { user, isLoading, setIsLoading } = useAuth({});
+  const [myevents, setMyevents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    if(user.email==null){
-        user.email=user?.uid;
-    }
+  if (user.email == null) {
+    user.email = user?.uid;
+  }
 
-    useEffect(()=>{
-        fetch(`https://forhumanity-server.herokuapp.com/myevents?email=${user.email}`,{
-            headers:{
-                'authorization':`Bearer ${localStorage.getItem('idToken')}`
-            }
-        })
-        .then(res=>{
-            if(res.status===200){
-                return res.json()
-            }
-            else if(res.status===401){
-                navigate('/login')
-            }
-        })
-        .then(data=>{
-            setMyevents(data)
-            setLoading(false)
-        })
-    },[])
-console.log(myevents);
-    
-//console.log(user?.email);
-    
+  useEffect(() => {
+    fetch(
+      `https://forhumanity-server.herokuapp.com/myevents?email=${user.email}`,
+      {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("idToken")}`,
+        },
+      }
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else if (res.status === 401) {
+          navigate("/login");
+        }
+      })
+      .then((data) => {
+        setMyevents(data);
+        setLoading(false);
+      });
+  }, []);
+  console.log(myevents);
 
-    return (
-        <div className='container-fluid' style={{minHeight:"73vh"}}>
-            <img className='user-dp mt-5' src={user?.photoURL} alt="" />
-            <h1 className='text-center my-3'>{user?.displayName.toUpperCase()}</h1>
-            { loading?<LoadingSpinner/>:myevents.length === 0 ? (
-          <div
-            className="text-center"
-            style={{ minHeight: "5vh" }}
-          >
-            <h1 className="text-center" style={{ color: "gray" }}>
-              You didn't register to any event! Please Register ! ! !
-            </h1>
-            <Link to='/register'> Go to Register </Link>
-          </div>
-        ):(
-                 <div className='row container-fluid gy-5 my-5'>
-                    {
-                        myevents?.map(myevent=><UserCard key={myevent._id} myevent={myevent}></UserCard>
-                        )
-                    }
-                 </div>
-            )
-               
-            }
+  //console.log(user?.email);
+
+  return (
+    <div className="container-fluid" style={{ minHeight: "73vh" }}>
+      <img className="user-dp mt-5" src={user?.photoURL} alt="" />
+      <h1 className="text-center my-3">{user?.displayName.toUpperCase()}</h1>
+      {loading ? (
+        <LoadingSpinner />
+      ) : myevents.length === 0 ? (
+        <div className="text-center" style={{ minHeight: "5vh" }}>
+          <h1 className="text-center" style={{ color: "gray" }}>
+            You didn't register to any event! Please Register ! ! !
+          </h1>
+          <Link to="/register"> Go to Register </Link>
         </div>
-    );
+      ) : (
+        <div className="container">
+          <table class="table table-responsive table-striped text-center table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Event</th>
+                <th scope="col">Date</th>
+                <th scope="col">Email</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+            {myevents?.map((myevent,index) => (
+              <UserCard index={index} key={myevent._id} myevent={myevent}></UserCard>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default User;
